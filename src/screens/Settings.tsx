8c0caf95771774086b1d15936733c186fc3e1eeb
Herlan <herlanherbas@gmail.com>
2024-05-31 03:11:05 -0400

    create: screen Settings

---
+++ src/screens/Settings.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+type Props = {}
+
+const Settings:React.FC = () => {
+  return (
+    <View>
+        <Text>Settings Screen</Text>
+    </View>
+  )
+}
+
+export default Settings
